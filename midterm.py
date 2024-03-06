6f9928b39626c4454bd3c84ea433fa15cbcd02ff
Eli Orians <eli.orians@gmail.com>
2024-03-06 14:14:02 -0500

    start

---
+++ midterm.py
@@ -0,0 +1,22 @@
+
+#part 1
+def plot():
+    print("plot")
+
+#part 2
+def golden():
+    print("golden")
+
+#part 3
+def fibonacci():
+    print("fib")
+
+#part 4
+def newtons():
+    print("new")
+
+def main():
+    print("")
+
+if __name__ == "__main__":
+    main()
